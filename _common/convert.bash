4d403adb7b7a32f709fb0e2c510c51e12e311170
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-02-20 01:12:21 +0900

    convert.bash: use 4 spaces for indentation

--- _common/convert.bash
+++ _common/convert.bash
@@ -24,23 +24,23 @@ bpy.context.scene.render.filepath = output_path
 PYTHON
 
 svg2png () {
-  inkscape -z "$1" -e "${1%.*}".png 2>/dev/null >/dev/null
+    inkscape -z "$1" -e "${1%.*}".png 2>/dev/null >/dev/null
 }; export -f svg2png
 
 mmpz2wav () {
-  lmms --format wav --render "$1" >/dev/null 2>/dev/null
+    lmms --format wav --render "$1" >/dev/null 2>/dev/null
 }; export -f mmpz2wav
 
 kra2png () {
-  krita --export --export-filename "${1%.*}".png "$1" 2>/dev/null >/dev/null
+    krita --export --export-filename "${1%.*}".png "$1" 2>/dev/null >/dev/null
 }; export -f kra2png
 
 render_blender () {
-  echo "render_blender: $1"...
-  blender -b "$1" -a 2>/dev/null >/dev/null
+    echo "render_blender: $1"...
+    blender -b "$1" -a 2>/dev/null >/dev/null
 }; export -f render_blender
 
 render_blender_marker () {
-  echo "render_blender_marker: $1"
-  blender -b "$1" --python <(echo "$render_marker") 2>/dev/null >/dev/null
+    echo "render_blender_marker: $1"
+    blender -b "$1" --python <(echo "$render_marker") 2>/dev/null >/dev/null
 }; export -f render_blender_marker
